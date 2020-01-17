92846d7cb00761be3e58724375547b34a36170e5
maxpowernz <aaronguilford@outlook.com>
2020-01-17 22:00:33 +1300

    comiit

--- battleship/Program.cs
+++ battleship/Program.cs
@@ -4,17 +4,7 @@ using System.Linq;
 
 namespace BattleShip
 {
-    /*
- * 
- * Your challenge is to implement this simplified game of Battleships using text input and output.
-The computer randomly chooses the location of two single-cell "ships" on a board of 8 by 8 cells.The user then has 20 guesses to find the two ships.
-The user enters a co-ordinate, for example 3,5, and the computer locates the nearest ship to that co-ordinate and tells them they're "hot" if they're 1 to 2 cells away, "warm" if they're 3 to 4 cells away, or "cold" if they're further away.
-As an example, 3,5 is three cells away from 2,7 because (3 - 2) + (7 - 5) = 3, so they'd be told they were "warm".
-If the user correctly guesses a ship's location, they're told they've got a hit and that ship is removed from the board. The game ends when both ships have been hit by the user, or the user has used up their 20 guesses.
-Write your code in a style that you consider to be production quality.
-Remember: please do your working on this page and ensure all code is your own, no copying and pasting. It doesn't matter if your code doesn't run, we're more interested in your logical thinking, process and coding style. Select the language you'd like to use below to get
-=============================
-*/
+
     public class Ship
     {
         public int Row { get; set; }
